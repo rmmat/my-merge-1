def5f457856574fe3c7bd9bd0c37282c95e7068b
Richard Matsumoto <richard.matsumoto@gmail.com>
2021-05-16 17:57:50 -0400

    update Brass

--- src/orchestra/section/Brass.java
+++ src/orchestra/section/Brass.java
@@ -4,5 +4,8 @@ public class Brass extends Section {
     public Brass() {
         super("Brass");
         addInstrument("French Horn");
+        addInstrument("Trumpte");
+        addInstrument("Trombone");
+        addInstrument("Tuba");
     }
 }
